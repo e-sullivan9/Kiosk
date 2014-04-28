f35afe0ad60ea20e4d53d8bc8247fdfd5a51165f
Eric Sullivan <e_sullivan9@post.massbay.edu>
2014-04-28 15:38:11 -0400

    TTS stuff

---
+++ src/Backend/Specialist.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Backend;
+
+/**
+ *
+ * @author Eric Sullivan
+ */
+public class Specialist extends User{
+
+    public Specialist(boolean follow, String reason, String location, String email, String fName, String lname, String role, String phone) {
+        super(follow, reason, location, email, fName, lname, role, phone);
+    }
+    
+    
+}
